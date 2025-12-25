a6e1f49d1e1455f2b41318f101ada78153599d16
Xylitol <jam.xylitol@gmail.com>
2025-12-26 00:15:45 +0800

    feat: add updater plugin and related configurations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -103,6 +103,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(
             tauri_plugin_global_shortcut::Builder::new()
