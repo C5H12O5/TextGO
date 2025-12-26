33078f8ef3dc61da2625e573f4e77557a3813271
Xylitol <jam.xylitol@gmail.com>
2025-12-26 21:58:05 +0800

    feat: implement update check and download functionality

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -102,6 +102,7 @@ pub fn run() {
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_autostart::Builder::new().build())
